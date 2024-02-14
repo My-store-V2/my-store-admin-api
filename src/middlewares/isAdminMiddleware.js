1c55af5d5409f45ca498c7f1da2d37439a690e0a
AndreClaveria <andreclaveria2002@gmail.com>
2024-02-14 15:18:14 +0100

    middlewares admin, bug sur le token

--- src/middlewares/isAdminMiddleware.js
+++ src/middlewares/isAdminMiddleware.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("../models"); // Supposons que vous ayez déjà configuré Sequelize et importé le modèle User
+const db = require("../models"); // Supposons que vous ayez déjà configuré Sequelize et importé le modèle User
 
 const isAdminMiddleware = async (req, res, next) => {
     // Récupérer le token d'authentification depuis les en-têtes de la requête
     const token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkN2M1OTc5Ny1jYjQwLTExZWUtODM3MC00MjAxMGE0MDAwMDUiLCJpYXQiOjE3MDc5MTkyMzIsImV4cCI6MTcwNzkyMjgzMn0.pYb32OPafpKy23aoWgPfUoOWPYq9RRPswOkmEHsBPII";
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzMWZiNjY5NS1jYjQzLTExZWUtODM3MC00MjAxMGE0MDAwMDUiLCJpYXQiOjE3MDc5MjAyMzUsImV4cCI6MTcwNzkyMzgzNX0.LvrIiS1qu6beoTxMgImTPQ5A6Czr0ju9jx12XtagCQg";
 
     if (!token) {
         return res.status(401).json({ message: "Token non fourni" });
@@ -15,13 +15,15 @@ const isAdminMiddleware = async (req, res, next) => {
         const decoded = jwt.verify(token, "secretOrKey");
 
         // Recherchez l'utilisateur dans la base de données en utilisant l'ID décodé
-        const user = await User.findByPk(decoded.id);
-
+        const user = await db.User.findByPk(decoded.userId);
+        console.log(decoded);
+        console.log(user.admin);
         if (!user) {
             return res.status(401).json({ message: "Utilisateur non trouvé" });
         }
 
         // Vérifier si l'utilisateur est un administrateur
+
         if (!user.admin) {
             return res.status(403).json({
                 message: "Accès interdit - Vous devez être administrateur",
